da07dd09bff19f7d33274ca81db1cfd0cf3da3ba
amornsukgang <justince.gang@gmail.com>
2022-01-15 18:28:04 +0700

    Create ex4.py

---
+++ ex4.py
@@ -0,0 +1,10 @@
+FE = 90
+GB = 79
+IS = 80
+CP = 99
+
+print("--- Your Score ---")
+print("Foundation English : คะแนนที่ได้",FE)
+print("General Business : คะแนนที่ได้",GB)
+print("Introduction to Computer Systems : คะแนนที่ได้",IS)
+print("Computer Programming : คะแนนที่ได้",CP)
